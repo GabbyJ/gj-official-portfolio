4b12f93c9f47a73cdb50152665c1037a3c3a6f5a
gabrielle <gabrielle@Gabrielles-MacBook-Pro-2.local>
2020-08-03 01:48:56 -0500

    Updates

---
+++ precache-manifest.e808a86703ec3137ca92808bfb338890.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc266bdf422b302f6a53ddb5acb4221e",
+    "url": "/gj-official-portfolio/index.html"
+  },
+  {
+    "revision": "27b364b595c03801baa8",
+    "url": "/gj-official-portfolio/static/css/2.f681e662.chunk.css"
+  },
+  {
+    "revision": "63aa93c081a2a66af6da",
+    "url": "/gj-official-portfolio/static/css/main.1436cf5c.chunk.css"
+  },
+  {
+    "revision": "27b364b595c03801baa8",
+    "url": "/gj-official-portfolio/static/js/2.58567dbd.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/gj-official-portfolio/static/js/2.58567dbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63aa93c081a2a66af6da",
+    "url": "/gj-official-portfolio/static/js/main.c6c5250a.chunk.js"
+  },
+  {
+    "revision": "a7c8c0f100a498c70800",
+    "url": "/gj-official-portfolio/static/js/runtime-main.f21f71f6.js"
+  }
+]);
