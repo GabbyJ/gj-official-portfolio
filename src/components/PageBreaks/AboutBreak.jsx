2a1f5e57357b4bd4c993ba796d5f902e71216856
gabrielle <gabrielle@Gabrielles-MacBook-Pro-2.local>
2020-08-03 02:02:15 -0500

    Edit Desk image.

--- src/components/PageBreaks/AboutBreak.jsx
+++ src/components/PageBreaks/AboutBreak.jsx
@@ -4,7 +4,7 @@ function AboutBreak() {
     return(
         <div>
         <div>
-          <img className="techDesk" src="../../images/tech-desk.png" alt="" data-aos="fade-left" data-aos-duration="700"/>
+          <img className="techDesk" src="./images/tech-desk.png" alt="" data-aos="fade-left" data-aos-duration="700"/>
           <hr className="line" />
         </div>
         <hr className="line-bottom" />
