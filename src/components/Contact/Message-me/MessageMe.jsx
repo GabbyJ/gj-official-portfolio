a72cb0c790f8e4235b73d4681448fa6497a917f3
gabrielle <gabrielle@Gabrielles-MacBook-Pro-2.local>
2020-08-23 16:51:07 -0500

    1. Contact form works now. 2.Typed header doesn't break or cut off on mobile anymore. 3. Create Thank you page for contact form submission. 4. Created custom 404 error page.

--- src/components/Contact/Message-me/MessageMe.jsx
+++ src/components/Contact/Message-me/MessageMe.jsx
@@ -1,79 +1,57 @@
 import React from 'react';
 
-class MessageMe extends React.Component{
-  
-  constructor(props) {
-	super(props);
-	this.state = {
-  	name: '',
-  	email: '',
-  	message: ''
-	}
-  }
 
-  handleSubmit(e) {
-    e.preventDefault();
 
-    fetch('http://localhost:3002/send',{
-        method: "POST",
-        body: JSON.stringify(this.state),
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-      }).then(
-    	(response) => (response.json())
-       ).then((response)=>{
-      if (response.status === 'success'){
-        alert("Message Sent."); 
-        this.resetForm()
-      }else if(response.status === 'fail'){
-        alert("Message failed to send.")
-      }
-    })
-  }
 
-  resetForm(){
-    
-     this.setState({name: '', email: '', message: ''})
-  }
-  
-  render() {
-	return(
+export default function MessageMe() {
+  return (
     <section className="contact" id="contact">
     <h2 data-aos="fade-up" data-aos-duration="700">Message Me</h2>
     <div className="form" data-aos="fade-up" data-aos-duration="700">
-  	<form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST" action="mailto:gabbyj424@gmail.com" className="login-form">
-  	<div className="form-group" data-aos="fade-up" data-aos-duration="700">
-      	<label htmlFor="name">Name</label>
-      	<input type="text" className="form-control" id="name" placeholder="Name" required="required" value={this.state.name} onChange={this.onNameChange.bind(this)} />
+    <form id="contact-form" method="POST" action="https://formsubmit.co/fddbb2df13653f25023d42513795606c" className="login-form">
+    <input type="hidden" name="_next" value="http://localhost:3000/thanks.html" />
+    <input type="hidden" name="_subject" value="Enquiry from gmjacobsdev.com" />
+  	<div className="form-group" >
+      	<label>Name</label>
+      	<input
+          type="text" 
+          className="form-control" 
+          id="name" 
+          name="Name" 
+          placeholder="Name" 
+          required="required"
+          aria-label="Name"
+        />
   	</div>
-  	<div className="form-group" data-aos="fade-up" data-aos-duration="700">
-      	<label htmlFor="exampleInputEmail1">Email address</label>
-      	<input type="email" className="form-control" id="email" placeholder="Email" required="required" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
+  	<div className="form-group" >
+      	<label>Email address</label>
+      	<input 
+          type="email" 
+          className="form-control" 
+          id="email" 
+          name="Email" 
+          placeholder="Email" 
+          required="required" 
+          aria-label="Email"
+        />
   	</div>
-  	<div className="form-group" data-aos="fade-up" data-aos-duration="700">
-      	<label htmlFor="message">Message</label>
-      	<textarea type="text" className="form-control" rows="5" id="message" placeholder="Message" required="required" defaultValue={""} value={this.state.message} onChange={this.onMessageChange.bind(this)} />
+  	<div className="form-group" >
+      	<label>Message</label>
+      	<textarea 
+          type="text" 
+          className="form-control"  
+          id="message" 
+          name="Message" 
+          placeholder="Message" 
+          required="required" 
+          aria-label="Enter your Message"
+          rows="5"
+        />
   	</div>
-  	<button type="submit" className="btn btn-primary" data-aos="fade-up" data-aos-duration="700">Submit</button>
-  	</form>
+  	<button type="submit" className="btn btn-primary" >Submit</button>
+    
+    </form>
     </div>
   	</section>
-	);
-  }
-
-  onNameChange(event) {
-	this.setState({name: event.target.value})
-  }
-
-  onEmailChange(event) {
-	this.setState({email: event.target.value})
-  }
-
-  onMessageChange(event) {
-	this.setState({message: event.target.value})
-  }
+  );
 }
-
-export default MessageMe;
